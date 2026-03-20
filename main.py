4babd0243cdff71514f2523d3c2b981b3a5fbca3
jakmate <jorzolek@hme.com>
2026-03-20 14:05:24 +0000

    Fix - After OSError sensor reconnected temp stuck at first reading

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-""" BME280 sensor data exported using FastAPI
+"""BME280 sensor data exported using FastAPI
 
 Routes:
     /metrics       Prometheus text-format
@@ -18,7 +18,6 @@ CLI Arguments:
     -h, --help
 
 Usage Examples:
-    default:
     uv run main.py -l 0.0.0.0 -p 9000 -c 5
 
 """
@@ -32,13 +31,6 @@ from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from smbus2 import SMBus
 import uvicorn
-
-print(""" Press Ctrl+C to exit! """)
-
-
-# Initialise the BME280
-bus = SMBus(1)
-bme280 = BME280(i2c_dev=bus)
 
 # Parse args
 parser = argparse.ArgumentParser(prog="main")
@@ -56,18 +48,20 @@ TTL = args.cache
 
 
 class State:
-    """ Tracks state of sensor """
+    """Tracks state of sensor"""
 
     def __init__(self):
+        self.bus = None
+        self.bme280 = None
         self.temp = None
         self.time = 0
 
     def get_json_state(self):
-        """ Return state as json """
+        """Return state as json"""
         return {"temp": state.temp, "time": state.time}
 
     def get_prometheus_state(self):
-        """ Returns prometheus formatted state """
+        """Returns prometheus formatted state"""
         return f"""# HELP rpi_temperature_celsius Temperature from BME280
                 # TYPE rpi_temperature_celsius gauge
                 rpi_temperature_celsius {state.temp}
@@ -76,16 +70,29 @@ class State:
                 rpi_timestamp_seconds {state.time}
                 """
 
+    def connect(self):
+        """Initialise BME280"""
+        self.bus = SMBus(1)
+        self.bme280 = BME280(i2c_dev=self.bus)
+
+    def disconnect(self):
+        """Reset the BME280 to default"""
+        self.bus.close()
+        self.bus = None
+        self.bme280 = None
+
 
 state = State()
 
 
 async def get_temperature():
-    """ Gets temperature using bme280 lib,
-    updates state and goes to sleep for TTL seconds """
+    """Gets temperature using bme280 lib,
+    updates state and goes to sleep for TTL seconds"""
     while True:
         try:
-            temperature = round(bme280.get_temperature(), 1)
+            if not state.bus:
+                state.connect()
+            temperature = round(state.bme280.get_temperature(), 1)
             print(f"{temperature}°C {int(asyncio.get_event_loop().time())}")
 
             state.temp = temperature
@@ -93,13 +100,14 @@ async def get_temperature():
 
         except OSError as e:
             print(f"An I/O error occured {e}")
+            state.disconnect()
 
         await asyncio.sleep(TTL)
 
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
-    """ Runs get_temperature until cancelled """
+    """Runs get_temperature until cancelled"""
     task = asyncio.create_task(get_temperature())
     yield
     task.cancel()
@@ -119,7 +127,7 @@ app.add_middleware(
 
 @app.middleware("http")
 async def add_headers(request: Request, call_next):
-    """ Adds security headers """
+    """Adds security headers"""
     response = await call_next(request)
 
     response.headers["Cache-Control"] = "no-store"
@@ -131,14 +139,14 @@ async def add_headers(request: Request, call_next):
 # JSON endpoint
 @app.get("api/metrics")
 async def get_json_metrics():
-    """ Returns json formatted state """
+    """Returns json formatted state"""
     return state.get_json_state()
 
 
 # Prometheus endpoint
 @app.get("/metrics")
 async def get_prometheus_metrics():
-    """ Returns prometheus formatted state """
+    """Returns prometheus formatted state"""
     return state.get_prometheus_state()
 
 
