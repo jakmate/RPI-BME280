b192433121b20c8d0b4e54a0e5b31163bd73d72a
jakmate <jorzolek@hme.com>
2026-03-30 15:21:01 +0100

    add pressure and humidity

--- main.py
+++ main.py
@@ -4,7 +4,10 @@
 
 Routes:
     /metrics       Prometheus text-format
-    /api/metrics   JSON format
+    /api/metrics   JSON state
+    /temperaure    JSON temperaute
+    /humidity      JSON humidity
+    /pressure      JSON pressure
 
 Default config:
     Host: 0.0.0.0
@@ -45,6 +48,8 @@ class State:
         self.bme280 = None
         self.ttl = None
         self.temp = None
+        self.humidity = None
+        self.pressure = None
         self.time = 0
 
     def get_temp(self):
@@ -54,6 +59,22 @@ class State:
     def set_temp(self, temperature):
         """Setter for temp"""
         self.temp = temperature
+
+    def get_humidity(self):
+        """Getter for humidity"""
+        return self.humidity
+
+    def set_humidity(self, humidity):
+        """Setter for humidity"""
+        self.humidity = humidity
+
+    def get_pressure(self):
+        """Getter for pressure"""
+        return self.pressure
+
+    def set_pressure(self, pressure):
+        """Setter for pressure"""
+        self.pressure = pressure
 
     def get_time(self):
         """Getter for time"""
@@ -86,16 +107,23 @@ class State:
 state = State()
 
 
-async def read_temperature():
-    """Reads temperature using bme280 lib,
+async def read_metrics():
+    """Reads metrics using bme280 lib,
     updates state and goes to sleep for TTL seconds"""
     while True:
         try:
             if not state.bus:
                 state.connect()
-                await asyncio.sleep(0.1)
+                await asyncio.sleep(1)
             state.set_temp(round(state.bme280.get_temperature(), 1))
+            state.set_humidity(round(state.bme280.get_humidity(), 1))
+            state.set_pressure(round(state.bme280.get_pressure(), 1))
             state.set_time(int(datetime.utcnow().timestamp()))
+            # print(
+            #    f"Temp: {state.get_temp()} Time: {state.get_time()}
+            #    Pressure: {state.get_pressure()}
+            #    Humidity: {state.get_humidity()}"
+            # )
 
         except OSError as e:
             print(f"An OS error occured: {e}")
@@ -114,8 +142,8 @@ async def read_temperature():
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
-    """Runs get_temperature until cancelled"""
-    task = asyncio.create_task(read_temperature())
+    """Runs get_metrics until cancelled"""
+    task = asyncio.create_task(read_metrics())
     yield
     task.cancel()
 
@@ -143,11 +171,43 @@ async def add_headers(request: Request, call_next):
     return response
 
 
-# JSON endpoint
+# JSON endpoints
 @app.get("/api/metrics")
 async def get_json_metrics():
     """Returns json formatted state"""
-    return {"temp": state.get_temp(), "time": state.get_time()}
+    return {
+        "temp": state.get_temp(),
+        "humidity": state.get_humidity(),
+        "pressure": state.get_pressure(),
+        "time": state.get_time(),
+    }
+
+
+@app.get("/api/temperature")
+async def get_json_temperature():
+    """Returns json formatted temperature"""
+    return {
+        "temp": state.get_temp(),
+        "time": state.get_time(),
+    }
+
+
+@app.get("/api/humidity")
+async def get_json_humidity():
+    """Returns json formatted humidity"""
+    return {
+        "humidity": state.get_humidity(),
+        "time": state.get_time(),
+    }
+
+
+@app.get("/api/pressure")
+async def get_json_pressure():
+    """Returns json formatted pressure"""
+    return {
+        "pressure": state.get_pressure(),
+        "time": state.get_time(),
+    }
 
 
 # Prometheus endpoint
@@ -155,11 +215,26 @@ async def get_json_metrics():
 async def get_prometheus_metrics():
     """Returns prometheus formatted state"""
     lines = []
-    lines.append("# HELP cc_metrics_ambient_temperature_celsius Temperature from BME280")
+    lines.append(
+        "# HELP cc_metrics_ambient_temperature_celsius Temperature from BME280"
+    )
     lines.append("# TYPE cc_metrics_ambient_temperature_celsius gauge")
     if state.get_temp() is not None:
         lines.append(f"cc_metrics_ambient_temperature_celsius {state.get_temp()}")
-    lines.append("# HELP cc_metrics_ambient_timestamp_seconds Unix timestamp when metric was sampled")
+
+    lines.append("# HELP cc_metrics_ambient_humidity_per Humidity from BME280")
+    lines.append("# TYPE cc_metrics_ambient_humidity_per gauge")
+    if state.get_humidity() is not None:
+        lines.append(f"cc_metrics_ambient_humidity {state.get_humidity()}")
+
+    lines.append("# HELP cc_metrics_ambient_pressure_hPa Pressure from BME280")
+    lines.append("# TYPE cc_metrics_ambient_pressure_hPa gauge")
+    if state.get_pressure() is not None:
+        lines.append(f"cc_metrics_ambient_pressure_hPa {state.get_pressure()}")
+
+    lines.append(
+        "# HELP cc_metrics_ambient_timestamp_seconds Unix timestamp when sampled"
+    )
     lines.append("# TYPE cc_metrics_ambient_timestamp_seconds gauge")
     lines.append(f"cc_metrics_ambient_timestamp_seconds {state.get_time()}\n")
 
