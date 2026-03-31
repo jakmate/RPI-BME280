df97d09bf5e9c9952f0809d825d5b157a1dc1a6d
jakmate <jorzolek@hme.com>
2026-03-31 09:43:01 +0100

    remove getters and setters

--- main.py
+++ main.py
@@ -52,45 +52,11 @@ class State:
         self.pressure = None
         self.time = 0
 
-    def get_temp(self):
-        """Getter for temp"""
-        return self.temp
-
-    def set_temp(self, temperature):
-        """Setter for temp"""
-        self.temp = temperature
-
-    def get_humidity(self):
-        """Getter for humidity"""
-        return self.humidity
-
-    def set_humidity(self, humidity):
-        """Setter for humidity"""
-        self.humidity = humidity
-
-    def get_pressure(self):
-        """Getter for pressure"""
-        return self.pressure
-
-    def set_pressure(self, pressure):
-        """Setter for pressure"""
-        self.pressure = pressure
-
-    def get_time(self):
-        """Getter for time"""
-        return self.time
-
-    def set_time(self, time):
-        """Setter for time"""
-        self.time = time
-
-    def get_ttl(self):
-        """Getter for ttl"""
-        return self.ttl
-
-    def set_ttl(self, ttl):
-        """Setter for ttl"""
-        self.ttl = ttl
+    def reset_metrics(self):
+        """Reset metrics to default values"""
+        self.temp = None
+        self.humidity = None
+        self.pressure = None
 
     def connect(self):
         """Initialise BME280"""
@@ -99,7 +65,8 @@ class State:
 
     def disconnect(self):
         """Reset the BME280 to default"""
-        self.bus.close()
+        if self.bus:
+            self.bus.close()
         self.bus = None
         self.bme280 = None
 
@@ -115,29 +82,26 @@ async def read_metrics():
             if not state.bus:
                 state.connect()
                 await asyncio.sleep(1)
-            state.set_temp(round(state.bme280.get_temperature(), 1))
-            state.set_humidity(round(state.bme280.get_humidity(), 1))
-            state.set_pressure(round(state.bme280.get_pressure(), 1))
-            state.set_time(int(datetime.utcnow().timestamp()))
-            # print(
-            #    f"Temp: {state.get_temp()} Time: {state.get_time()}
-            #    Pressure: {state.get_pressure()}
-            #    Humidity: {state.get_humidity()}"
-            # )
+
+            state.temp = round(state.bme280.get_temperature(), 1)
+            state.humidity = round(state.bme280.get_humidity(), 1)
+            state.pressure = round(state.bme280.get_pressure(), 1)
+            state.time = int(datetime.utcnow().timestamp())
 
         except OSError as e:
             print(f"An OS error occured: {e}")
             state.disconnect()
-            state.set_temp(None)
+            state.reset_metrics()
         except ZeroDivisionError as e:
             print(f"Division by zero occured: {e}")
-            state.set_temp(None)
+            state.disconnect()
+            state.reset_metrics()
         except Exception as e:
             print(f"New Exception {e}")
-            state.set_temp(None)
             state.disconnect()
+            state.reset_metrics()
 
-        await asyncio.sleep(state.get_ttl())
+        await asyncio.sleep(state.ttl)
 
 
 @asynccontextmanager
@@ -146,6 +110,11 @@ async def lifespan(_: FastAPI):
     task = asyncio.create_task(read_metrics())
     yield
     task.cancel()
+    try:
+        await task
+    except asyncio.CancelledError:
+        pass
+    state.disconnect()
 
 
 app = FastAPI(lifespan=lifespan)
@@ -176,10 +145,10 @@ async def add_headers(request: Request, call_next):
 async def get_json_metrics():
     """Returns json formatted state"""
     return {
-        "temp": state.get_temp(),
-        "humidity": state.get_humidity(),
-        "pressure": state.get_pressure(),
-        "time": state.get_time(),
+        "temp": state.temp,
+        "humidity": state.humidity,
+        "pressure": state.pressure,
+        "time": state.time,
     }
 
 
@@ -187,8 +156,8 @@ async def get_json_metrics():
 async def get_json_temperature():
     """Returns json formatted temperature"""
     return {
-        "temp": state.get_temp(),
-        "time": state.get_time(),
+        "temp": state.temp,
+        "time": state.time,
     }
 
 
@@ -196,8 +165,8 @@ async def get_json_temperature():
 async def get_json_humidity():
     """Returns json formatted humidity"""
     return {
-        "humidity": state.get_humidity(),
-        "time": state.get_time(),
+        "humidity": state.humidity,
+        "time": state.time,
     }
 
 
@@ -205,8 +174,8 @@ async def get_json_humidity():
 async def get_json_pressure():
     """Returns json formatted pressure"""
     return {
-        "pressure": state.get_pressure(),
-        "time": state.get_time(),
+        "pressure": state.pressure,
+        "time": state.time,
     }
 
 
@@ -219,24 +188,24 @@ async def get_prometheus_metrics():
         "# HELP cc_metrics_ambient_temperature_celsius Temperature from BME280"
     )
     lines.append("# TYPE cc_metrics_ambient_temperature_celsius gauge")
-    if state.get_temp() is not None:
-        lines.append(f"cc_metrics_ambient_temperature_celsius {state.get_temp()}")
+    if state.temp is not None:
+        lines.append(f"cc_metrics_ambient_temperature_celsius {state.temp}")
 
     lines.append("# HELP cc_metrics_ambient_humidity_per Humidity from BME280")
     lines.append("# TYPE cc_metrics_ambient_humidity_per gauge")
-    if state.get_humidity() is not None:
-        lines.append(f"cc_metrics_ambient_humidity {state.get_humidity()}")
+    if state.humidity is not None:
+        lines.append(f"cc_metrics_ambient_humidity {state.humidity}")
 
     lines.append("# HELP cc_metrics_ambient_pressure_hPa Pressure from BME280")
     lines.append("# TYPE cc_metrics_ambient_pressure_hPa gauge")
-    if state.get_pressure() is not None:
-        lines.append(f"cc_metrics_ambient_pressure_hPa {state.get_pressure()}")
+    if state.pressure is not None:
+        lines.append(f"cc_metrics_ambient_pressure_hPa {state.pressure}")
 
     lines.append(
         "# HELP cc_metrics_ambient_timestamp_seconds Unix timestamp when sampled"
     )
     lines.append("# TYPE cc_metrics_ambient_timestamp_seconds gauge")
-    lines.append(f"cc_metrics_ambient_timestamp_seconds {state.get_time()}\n")
+    lines.append(f"cc_metrics_ambient_timestamp_seconds {state.time}\n")
 
     content = "\n".join(lines)
 
@@ -250,7 +219,7 @@ def main(
     cache: Annotated[int, typer.Option(help="Cache TTL in seconds")] = 2,
 ):
     """Run fastapi app"""
-    state.set_ttl(cache)
+    state.ttl = cache
     uvicorn.run(app, host=host, port=port, reload=False)
 
 
